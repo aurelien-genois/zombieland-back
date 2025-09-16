acdb9b8e066801bff1a0d1ca1f939a87410e1cd9
MickSDonovan <michel.colacino@gmail.com>
2025-09-16 14:04:06 +0100

    🚧 build(app): Setup Zod + folder schemas
    Changelog: build

---
+++ src/schemas/utils.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+// ================================== VALIDATIONS =================================
+
+// --------------------  paramsId ------------------------
+const parseIdValidation = z.coerce
+  .number({ error: "Parameter is required" })
+  .int({ error: "Parameter should be a positif number!" })
+  .min(1, { error: "Parameter must be a positive integer" });
+
+// ================================== SCHEMAS =================================
+
+export const utilSchema = {
+  parseId: z.object({
+    id: parseIdValidation,
+  }),
+};

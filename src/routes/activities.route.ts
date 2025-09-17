728e8694a483d8d49ae318ca89ae910913808be6
aurelien-genois <aurelien.genois@gmail.com>
2025-09-17 16:37:37 +0200

    🌟 feat(app): get one Activity by id or slug: route/controller/zod-schema
    Changelog: feature

--- src/routes/activities.route.ts
+++ src/routes/activities.route.ts
@@ -5,6 +5,7 @@ import activitiesController from "../controllers/activities.controller.js";
 const router = Router();
 
 router.get("/", activitiesController.getAllActivities);
+router.get("/:slug", activitiesController.getOneActivity);
 router.post("/", activitiesController.createActivity);
 
 export default router;

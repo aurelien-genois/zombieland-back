dcfba15a1d3c831101156ab8f19d7acbf5983d50
aurelien-genois <aurelien.genois@gmail.com>
2025-09-19 15:56:26 +0200

    🔨 modify(app): get Activities: add public route for published activities
    Changelog: modify

--- src/routes/activities.route.ts
+++ src/routes/activities.route.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 
 import activitiesController from "../controllers/activities.controller.js";
+import { checkRoles } from "../middlewares/check-roles.middleware.js";
 
 const router = Router();
 
-router.get("/", activitiesController.getAllActivities);
+// only admin users can access all activities
+router.get(
+  "/",
+  checkRoles(["admin"]),
+  activitiesController.getAllActivities.bind(null, {})
+);
+
+// everyone can access published activities
+router.get(
+  "/published",
+  activitiesController.getAllActivities.bind(null, { status: "published" })
+);
+
+// /:slug after /published to avoid conflict
 router.get("/:slug", activitiesController.getOneActivity);
+
 router.post("/", activitiesController.createActivity);
 router.patch("/:id", activitiesController.updateActivity);
 router.delete("/:id", activitiesController.deleteActivity);

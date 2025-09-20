9b058b3b96d39f02d5feb15e38390f25ab0dd605
MickSDonovan <michel.colacino@gmail.com>
2025-09-20 20:11:08 +0100

    🌟 feat(app): Implementation Delete User By Admin or User and Update Role by admin
    Changelog: feature

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,8 +5,7 @@ import { checkRoles } from "../middlewares/check-roles.middleware.js";
 
 const router = Router();
 
-// --------------------  Get All Users --------------------
-router.get("/", checkRoles(["admin"]), usersController.getAllUsers);
+// ====================  MEMBER ROUTES ========================
 
 // --------------------  Get My Account --------------------
 router.get(
@@ -22,14 +21,32 @@ router.patch(
   usersController.updateUserPassword
 );
 
-// --------------------  Get One User --------------------
-router.get("/:id", checkRoles(["admin"]), usersController.getOneUser);
-
 // --------------------  Update Info --------------------
 router.patch(
   "/",
   checkRoles(["admin", "member"]),
   usersController.updateUserInfo
 );
+
+// --------------------  Delete User --------------------
+router.delete("/", checkRoles(["admin", "member"]), usersController.deleteUser);
+
+// ====================  ADMIN ROUTES ========================
+
+// --------------------  Get All Users --------------------
+router.get("/", checkRoles(["admin"]), usersController.getAllUsers);
+
+// --------------------  Get One User --------------------
+router.get("/:id", checkRoles(["admin"]), usersController.getOneUser);
+
+// --------------------  Update Role User --------------------
+router.patch(
+  "/:id/role",
+  checkRoles(["admin"]),
+  usersController.updateRoleUser
+);
+
+// --------------------  Delete User Account --------------------
+router.delete("/:id", checkRoles(["admin"]), usersController.deleteUserAccount);
 
 export default router;

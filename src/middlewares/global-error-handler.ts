5a68386e2d832b75c58ce0aa3697a5cb7166aadd
aurelien-genois <aurelien.genois@gmail.com>
2025-09-23 11:36:18 +0200

    📚 docs(app): comment on Global Error Handler
    Changelog: documentation

--- src/middlewares/global-error-handler.ts
+++ src/middlewares/global-error-handler.ts
@@ -6,7 +6,7 @@ export function globalErrorHandler(
   error: Error,
   _req: Request,
   res: Response,
-  _next: NextFunction
+  _next: NextFunction // don't remove ! it is required by express
 ) {
   const isProduction = process.env.NODE_ENV === "production";
   const baseError = {

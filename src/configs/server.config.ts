dc9920201cc1acb435d9e181a9f3b602565be93f
MickSDonovan <michel.colacino@gmail.com>
2025-09-27 15:27:15 +0100

    🐛 fix(app): Add verification is role existe for prevent error refresh cookie
    Changelog: fix

--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -1,8 +1,11 @@
+import { log } from "console";
+
 export const config = {
   server: {
     port: parseInt(process.env.PORT || "3000"),
     allowedOrigins: process.env.ALLOWED_ORIGINS || "*",
     jwtSecret: process.env.JWT_SECRET || "jwt-secret",
     secure: process.env.NODE_ENV === "production" || false,
+    logLevel: process.env.LOG_LEVEL || "info",
   },
 };

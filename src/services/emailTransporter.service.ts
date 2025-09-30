903fdbbf3e1bdecc618af9aa825111858bc3c5ea
MickSDonovan <michel.colacino@gmail.com>
2025-09-30 21:47:30 +0100

    🌟 feat(app): Remove mailpit for the first test with true email adress
    Changelog: feature

--- src/services/emailTransporter.service.ts
+++ src/services/emailTransporter.service.ts
@@ -4,4 +4,8 @@ export const emailTransporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
   secure: false,
+  auth: {
+    user: process.env.FROM_EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
 });

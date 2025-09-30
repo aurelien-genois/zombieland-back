b53dbd579886345a8a4c0187190d75e09264f104
MickSDonovan <michel.colacino@gmail.com>
2025-09-30 18:27:49 +0100

    🚧 build(app): SetUp Integration Test with compose.test and gloabl-setup with node:test
    Changelog: build

---
+++ src/routes/health.route.spec.test.ts
@@ -0,0 +1,13 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+describe("[GET] /api/health", () => {
+  it("should return a healthy status", async () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    assert.ok(true);
+  });
+});
---
+++ test/config/global-setup.ts
@@ -0,0 +1,78 @@
+import { after, before, beforeEach } from "node:test";
+import path from "node:path";
+import { $ } from "zx";
+import { Server } from "node:http";
+import { app } from "../../src/app.js";
+import { prisma } from "../../src/models/index.js";
+
+// ================================================================================
+// Objectif de ce fichier : mettre en place l'environnement des tests d'intégration
+
+// === AVANT de lancer les test ===
+// Créer une BDD de test (oquiztest)
+// Charger les variables d'environnement (.env.test) à l'aide du flag --env-file
+// Créer les tables dans cette BDD de test (run les migrations)
+// Lancer le serveur Express
+
+// === Entre chaque test ===
+// On vide les tables
+
+// === APRES les tests ===
+// Deconnecter le code de la BDD
+// Eteindre le serveur Express
+// Supprimer la BDD
+// ================================================================================
+
+// Serveur HTTP (de test)
+let server: Server;
+
+// Hook before : s'exécute une fois avant l'ensemble des tests
+before(async () => {
+  // Lancer la BDD oquiztest via un docker compose
+  const composeFileAbsolutePath = path.resolve(
+    import.meta.dirname,
+    "compose.test.yml"
+  );
+  await $`docker compose -f ${composeFileAbsolutePath} -p zombieland-test-db up -d`;
+  await $`sleep 1`; // Attendre une petite seconde pour s'assurer qu'elle tourne bien
+
+  // Créer les tables dans cette BDD
+  const prismaSchemaAbsolutePath = path.resolve(
+    import.meta.dirname,
+    "../../src/models/schema.prisma"
+  );
+  await $`npx prisma db push --schema=${prismaSchemaAbsolutePath}`;
+
+  // On lance un serveur de test
+  server = app.listen(process.env.PORT);
+});
+
+// Hook beforeEach : s'exécute une fois avant chaque test
+beforeEach(async () => {
+  await truncateTables();
+});
+
+// Hook after : s'exécute une fois après l'ensemble des tests
+after(async () => {
+  // On éteint le serveur HTTP
+  server.close();
+
+  // On deconnecte la connexion à la BDD
+  await prisma.$disconnect();
+
+  // On éteint la base de données de test
+  await $`docker compose -p zombieland-test-db down`;
+});
+
+async function truncateTables() {
+  // https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
+  await prisma.$executeRawUnsafe(`
+    DO $$ DECLARE
+      r RECORD;
+    BEGIN
+      FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'TRUNCATE TABLE "' || r.tablename || '" RESTART IDENTITY CASCADE';
+      END LOOP;
+    END $$;
+  `);
+}
